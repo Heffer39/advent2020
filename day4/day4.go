6e52d46e513bda84b8b1a2e8071f5e61f2c17585
Ryan Cerankowski <rcerankowski@gmail.com>
2020-12-22 12:14:07 -0500

    Day four part two

--- day4/day4.go
+++ day4/day4.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -49,24 +51,19 @@ func main() {
 	fieldsMap := make(map[string]string)
 
 	for i, s := range data {
-		//fmt.Println(s)
 		fields := strings.Split(s, " ")
-
 		for _, f := range fields {
 			split := strings.Split(f, ":")
 			if len(split) > 1 {
 				fieldsMap[split[0]] = split[1]
-				//fmt.Printf("var: %v, %v\n", split[0], split[1])
 			}
 		}
 
 		if s == "" || i == len(data)-1 {
 			if !validatePassport(fieldsMap) {
-				//fmt.Printf("invalid passport! %v\n", fieldsMap)
 				fieldsMap = make(map[string]string)
 				continue
 			}
-			//fmt.Printf("new passport!\n")
 			p := passport{
 				byr: fieldsMap["byr"],
 				iyr: fieldsMap["iyr"],
@@ -84,20 +81,77 @@ func main() {
 		}
 	}
 
-	for _, p := range passports {
-		fmt.Printf("%v\n", p)
-	}
-
 	fmt.Printf("valid passports: %v", validPassports)
 }
 
 func validatePassport(fieldsMap map[string]string) (validPassport bool) {
 	validPassport = true
 	for _, s := range requiredFields {
-		if fieldsMap[s] == "" {
+		val := fieldsMap[s]
+		if val == "" {
+			return false
+		}
+		switch s {
+		case "byr":
+			birthYear, _ := strconv.Atoi(val)
+			if len(val) != 4 || birthYear < 1920 || birthYear > 2002 {
+				fmt.Printf("invalid byr: %v\n", val)
+				return false
+			}
+		case "iyr":
+			issueYear, _ := strconv.Atoi(val)
+			if len(val) != 4 || issueYear < 2010 || issueYear > 2020 {
+				fmt.Printf("invalid iyr: %v\n", val)
+				return false
+			}
+		case "eyr":
+			expirationYear, _ := strconv.Atoi(val)
+			if len(val) != 4 || expirationYear < 2020 || expirationYear > 2030 {
+				fmt.Printf("invalid eyr: %v\n", val)
+				return false
+			}
+		case "hgt":
 			validPassport = false
-			//fmt.Printf("failed check! %v\n", s)
-			break
+			if strings.Contains(val, "in") {
+				num, _ := strconv.Atoi(strings.Split(val, "in")[0])
+				if num >= 59 && num <= 76 {
+					validPassport = true
+				}
+			}
+			if strings.Contains(val, "cm") {
+				num, _ := strconv.Atoi(strings.Split(val, "cm")[0])
+				if num >= 150 && num <= 193 {
+					validPassport = true
+				}
+			}
+			if !validPassport {
+				fmt.Printf("invalid hgt: %v\n", val)
+				return false
+			}
+		case "hcl":
+			if len(val) != 7 || val[0] != '#' || !regexp.MustCompile(
+				`^[a-f0-9]+$`).MatchString(strings.SplitN(val, "#", 2)[1]) {
+				fmt.Printf("invalid hcl: %v\n", val)
+				return false
+			}
+		case "ecl":
+			validColors := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+			validPassport = false
+			for _, c := range validColors {
+				if c == val {
+					validPassport = true
+					break
+				}
+			}
+			if validPassport == false {
+				fmt.Printf("invalid ecl: %v\n", val)
+				return false
+			}
+		case "pid":
+			if len(val) != 9 || !regexp.MustCompile(`^[0-9]+$`).MatchString(val) {
+				fmt.Printf("invalid pid: %v\n", val)
+				return false
+			}
 		}
 	}
 	return
