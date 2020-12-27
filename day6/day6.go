8257339b549e96c580278001e4d9c8015b8d0e95
Ryan Cerankowski <rcerankowski@gmail.com>
2020-12-27 15:48:26 -0500

    Day six part two

--- day6/day6.go
+++ day6/day6.go
@@ -10,29 +10,45 @@ import (
 func main() {
 	data := lib.ReadFile("questions.txt")
 
-	questions := make(map[rune]bool)
-	var questionCounts []int
+	questions := make(map[rune]int)
+	var anyoneYesCount []int
+	var everyoneYesCount []int
+	var groupSize int
 
 	for i, s := range data {
+		groupSize++
 		for _, c := range s {
 			if !unicode.IsLetter(c) {
 				log.Fatal("incorrect input")
 			}
-			questions[c] = true
+			questions[c]++
 		}
 		if s == "" || i == len(data)-1 {
-			count := 0
-			for range questions {
-				count++
+			anyoneCount := 0
+			everyoneCount := 0
+			for _, v := range questions {
+				anyoneCount++
+				if v == groupSize-1 || (v == groupSize && i == len(data)-1) {
+					everyoneCount++
+				}
 			}
-			questionCounts = append(questionCounts, count)
-			questions = make(map[rune]bool)
+			if anyoneCount != 0 {
+				anyoneYesCount = append(anyoneYesCount, anyoneCount)
+			}
+			if everyoneCount != 0 {
+				everyoneYesCount = append(everyoneYesCount, everyoneCount)
+			}
+			questions = make(map[rune]int)
+			groupSize = 0
 			continue
 		}
 	}
-	var sum int
-	for _, v := range questionCounts {
-		sum += v
+	var anyoneSum, everyoneSum int
+	for _, v := range anyoneYesCount {
+		anyoneSum += v
 	}
-	fmt.Println(sum)
+	for _, v := range everyoneYesCount {
+		everyoneSum += v
+	}
+	fmt.Printf("anyone: %v, everyone: %v\n", anyoneSum, everyoneSum)
 }
